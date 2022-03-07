992f4729ab2402e4db864525f9e1914fa63eef7b
implodingduck <zsek320@gmail.com>
2022-03-07 16:02:50 -0600

    explosions

--- function/negotiate/index.js
+++ function/negotiate/index.js
@@ -1,4 +1,11 @@
-module.exports = async function (context, req, connection) {
+const { WebPubSubServiceClient } = require("@azure/web-pubsub");
+
+module.exports = async function (context, req) {
+    const serviceClient = new WebPubSubServiceClient("<ConnectionString>", "<hubName>");
+
+    // Get the access token for the WebSocket client connection to use
+    let token = await serviceClient.getClientAccessToken();
+
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.res = { body: connection };
+    context.res = { body: token };
 };
--- terraform/main.tf
+++ terraform/main.tf
@@ -63,11 +63,30 @@ resource "azurerm_application_insights" "app" {
   workspace_id = data.azurerm_log_analytics_workspace.default.id
 }
 
+resource "azurerm_user_assigned_identity" "runas" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  name = "azurestaticwebpubsub-uai"
+}
+
+resource "azurerm_role_assignment" "pubsub" {
+  scope                = azurerm_resource_group.rg.id
+  #Web PubSub Service Owner (Preview)
+  role_definition_id  = "/providers/Microsoft.Authorization/roleDefinitions/12cf5a90-567b-43ae-8102-96cf46c7d9b4"
+  principal_id         = azurerm_user_assigned_identity.runas.principal_id
+}
 
 resource "azurerm_static_site" "swa" {
   name                = "azurestaticwebpubsub"
   resource_group_name = azurerm_resource_group.rg.name
   location            = "eastus2"
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.runas.principal_id
+    ]
+  }
   tags = merge(local.tags, { "hidden-link: /app-insights-instrmentation-key": azurerm_application_insights.app.instrumentation_key, "hidden-link: /app-insights-resource-id": replace(azurerm_application_insights.app.id, "Microsoft.Insights", "microsoft.insights") })
 }
 
@@ -90,6 +109,8 @@ resource "azurerm_web_pubsub" "pubsub" {
   }
 }
 
+
+
 resource "azurerm_web_pubsub_hub" "uhb" {
   name          = "pubsubhub"
   web_pubsub_id = azurerm_web_pubsub.pubsub.id
@@ -108,4 +129,17 @@ resource "azurerm_web_pubsub_hub" "uhb" {
     azurerm_web_pubsub.pubsub,
     azurerm_static_site.swa
   ]
+}
+
+resource "null_resource" "app_setting_cs"{
+  depends_on = [
+    azurerm_web_pubsub.pubsub,
+    azurerm_static_site.swa
+  ]
+  triggers = {
+    index = "${azurerm_static_site.swa.name}-${azurerm_web_pubsub.pubsub.name}"
+  }
+  provisioner "local-exec" {
+    command     = "az staticwebapp appsettings set --name ${azurerm_static_site.swa.name} --setting-names PUBSUBHUB_CONNECTIONSTR=\"Endpoint=https://${azurerm_web_pubsub.pubsub.name}.webpubsub.azure.com;Version=1.0;\" PUBSUBHUB_NAME=\"${azurerm_web_pubsub.pubsub.name}\""
+  }
 }
