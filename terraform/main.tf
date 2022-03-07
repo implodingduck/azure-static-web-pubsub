9375ba15ce14e3314e07e6a2b2dd10a3d704ffed
implodingduck <zsek320@gmail.com>
2022-03-07 12:29:45 -0600

    needs to us eastus2

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_static_site" "swa" {
   name                = "azurestaticwebpubsub"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = "eastus2"
   tags = local.tags
 }
 
